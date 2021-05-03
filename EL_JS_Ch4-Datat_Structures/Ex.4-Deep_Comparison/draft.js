a654d94103e6b9a45640936894e24a368cb2e7f0
Mohamed Hossam <m.hossameldine@gmail.com>
2021-05-03 12:40:37 +0200

    Thinking Draft

---
+++ EL_JS_Ch4-Datat_Structures/Ex.4-Deep_Comparison/draft.js
@@ -0,0 +1,17 @@
+function deepEqual (obOne, obTwo) {
+  let output;
+  arrOne = object.keys(obOne).sort();
+  arrTwo = object.keys(obTwo).sort();
+
+  if(typeof obOne == typeof obTwo == object 
+     && arrOne != null) {
+    if(arrOne.length != arrTwo.lenght 
+       || JSON.stringify(arrOne) != JSON.stringify(arrTwo) ) {
+      output = false;
+    } else {
+      for(let i = 0; i < arrOne.lenght;i++){
+       
+      }
+    }
+  }
+}
